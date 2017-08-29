2df8a06a9ce2dddbf47b8916d4617d2636984b53
zuoyan <zuoyan_njupt@163.com>
2017-08-29 23:28:16 +0800

    study

--- webpack-sample-project/webpack.config.js
+++ webpack-sample-project/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'eval-source-map',
@@ -9,27 +10,38 @@ module.exports = {
     filename: "bundle.js"//打包后输出文件的文件名
   },
 
-    module: {
-        rules: [
-            {
-                test: /(\.jsx|\.js)$/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "es2015", "react"
-                        ]
-                    }
-                },
-                exclude: /node_modules/
-            }
-        ]
-    },
+  module: {
+      rules: [
+          {
+              test: /(\.jsx|\.js)$/,
+              use: {
+                  loader: "babel-loader",
+              },
+              exclude: /node_modules/
+          },
+          {
+            test: /\.css$/,
+            use:[
+              {
+                loader:"style-loader"
+              },{
+                loader:"css-loader",
+                options:{
+                  module: true
+                }
+              }
+            ]
+          }
+      ]
+  },
 
   devServer: {
     contentBase: "./public",//本地服务器所加载的页面所在的目录
-    colors: true,//终端中输出结果为彩色
     historyApiFallback: true,//不跳转
     inline: true//实时刷新
-  } 
+  },
+
+  plugins: [
+    new webpack.BannerPlugin('版权所有，翻版必究')
+  ]
 }
