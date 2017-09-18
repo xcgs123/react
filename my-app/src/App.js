bd08c51c3960b6dc9f5cbb0e61c94579fd9a6f1f
zuoyan <zuoyan_njupt@163.com>
2017-09-18 17:47:20 +0800

    切角

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <ul>
-        <li><Link to="/about">11About</Link></li>
+        <li><Link to="/about">tab组件</Link></li>
         <li><Link to="/reactDemo">2reactDemo</Link></li>
         <li><Link to="/pullRefresh">下拉刷新</Link></li>
       </ul>
--- my-app/src/containers/PullRefresh/index.js
+++ my-app/src/containers/PullRefresh/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
+import './index.less';
 
 
 class PullRefresh extends Component{
@@ -10,9 +11,17 @@ class PullRefresh extends Component{
       }
   }
 
+  doRefresh(){
+    return (<div>1111</div>)
+  }
+
   render(){
     return(
-      <div>1</div>
+      <div className="refresh">
+        <div onClick={this.doRefresh}>点击刷新</div>
+
+        <div className='containers'>啦啦啦啦啦啦啦啦啦啦</div>
+      </div>
     )
   }
 }
